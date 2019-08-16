3f9d4cac05d59ab3e6056c342797df943867051c
magersoft <magervlad@yandex.ru>
2019-08-16 15:36:34 +0300

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basic';
+  posts: Post[] = [
+    { title: 'I want learning Angular Components', text: 'Text text text', id: 1 },
+    { title: 'Next block post', text: 'Text text text again text', id: 2 },
+  ];
+
+  updatePosts(post: Post) {
+    this.posts.unshift(post);
+  }
 }
